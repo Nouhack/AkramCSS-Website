942c12dd34f1fad413a956f788c33133b8719de7
Nouhack <nouh.saiche@gmail.com>
2024-07-20 18:53:22 +0200

    comment the fake algolia credentials

--- Components/search.tsx
+++ Components/search.tsx
@@ -2,10 +2,16 @@ import { DocSearch } from "@docsearch/react";
 
 function App() {
   return (
+        {
+            /* 
+             * those are fake credentials
+             *
+             * */
+        }
     <DocSearch
-      //appId="S4DPKX78ZX"
-      //indexName="akramcss"
-      //apiKey="51a2e29b6bd2757794a1bb91e4911a4d"
+      appId="S4DPKX78ZX"
+      indexName="akramcss"
+      apiKey="51a2e29b6bd2757794a1bb91e4911a4d"
     />
   );
 }
