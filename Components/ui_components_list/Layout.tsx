a268de0b193b76071279761a73a1d5f2386187c4
Nouhack <nouh.saiche@gmail.com>
2024-07-20 19:02:35 +0200

    fix typescript typos

--- Components/ui_components_list/Layout.tsx
+++ Components/ui_components_list/Layout.tsx
@@ -22,7 +22,7 @@ tomorrow,
     a11yLight
 } from "react-code-blocks";
 
-export default function Layout({code,Ui,title}) {
+export default function Layout({code:string,Ui:React.Component,title:string}) {
     const [show_code, setShow_code] = React.useState(false)
   return (
 
