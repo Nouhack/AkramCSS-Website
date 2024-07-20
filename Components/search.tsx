2e0baa1420dd0f9889c976beb3d7f71ce021766e
Nouhack <nouh.saiche@gmail.com>
2024-07-20 19:00:45 +0200

    empty algolia credentials

--- Components/search.tsx
+++ Components/search.tsx
@@ -2,16 +2,10 @@ import { DocSearch } from "@docsearch/react";
 
 function App() {
   return (
-        {
-            /* 
-             * those are fake credentials
-             *
-             * */
-        }
     <DocSearch
-      appId="S4DPKX78ZX"
-      indexName="akramcss"
-      apiKey="51a2e29b6bd2757794a1bb91e4911a4d"
+      appId=""
+      indexName=""
+      apiKey=""
     />
   );
 }
